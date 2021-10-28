e1574c897d39ec9481c29cdc0a16a2db518e18ec
jonahhhhhh <91928902+jonahhhhhh@users.noreply.github.com>
2021-10-28 11:08:43 -0500

    Rolling hard coded d6

--- src/app/die-roller/die-roller.component.ts
+++ src/app/die-roller/die-roller.component.ts
@@ -12,4 +12,12 @@ export class DieRollerComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  sideCount = "6"
+  rollValue = 0;
+
+  roll() {
+    this.rollValue = Math.floor(Math.random() * 6) + 1
+  }
+
+
 }
